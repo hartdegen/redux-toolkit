f7c987131ed8d9543a366a01ee7979439d0b747b
hartdegen <hartdegen182@gmail.com>
2022-04-26 20:46:38 +0300

    redux integration

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// Хуки находятся в react-redux
+import { useSelector, useDispatch } from 'react-redux';
+// Импортируем нужные действия
+import { decrement, increment } from '../slices/counterSlice.js';
+
+export default () => {
+  // Вытаскиваем данные из хранилища. state – все состояние
+  const count = useSelector((state) => state.counter.value);
+  // Возвращает метод store.dispatch() текущего хранилища
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Прибавить
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Отнять
+        </button>
+      </div>
+    </div>
+  );
+};
